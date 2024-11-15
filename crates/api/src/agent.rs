77c0a68fbdf12f1eafdc7ff3dcd1020a0f1ba618
neonphog <neonphog@gmail.com>
2024-11-15 10:23:10 -0700

    wip

--- crates/api/src/agent.rs
+++ crates/api/src/agent.rs
@@ -2,74 +2,54 @@
 
 use crate::*;
 
-/// Additional metadata associated with an agent.
-/// This struct represents the extensibility of agent info,
-/// everything in here must be optional or provide a sane default.
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct AgentInfoMetadata {
-    /// If set, this indicates the primary url at which this agent may
-    /// be reached. This should largely only be UNSET if this is a tombstone.
-    pub url1: Option<String>,
-
-    /// If unset, this agent is claiming a zero storage arc,
-    /// that is, they are saying they store nothing.
-    /// If set, this indicates the inclusive bounds which this agent
-    /// claims they are an authority for storage. Note, if the first
-    /// bound is after the second bound, that means the claim wraps
-    /// around the end of u32::MAX to the other side.
-    pub storage_arc1: Option<(u32, u32)>,
+/// Defines a type capable of cryptographic signatures.
+pub trait Signer {
+    /// Sign the encoded data, returning the resulting detached signature bytes.
+    fn sign(
+        &self,
+        agent_info: &AgentInfo,
+        message: &[u8],
+    ) -> BoxFut<'_, std::io::Result<bytes::Bytes>>;
 }
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct AgentInfoStringTimestamps {
-    agent: AgentId,
-    space: SpaceId,
-    created_at: String,
-    expires_at: String,
-    is_tombstone: bool,
-    metadata: AgentInfoMetadata,
+/// Defines a type capable of cryptographic verification.
+pub trait Verifier {
+    /// Verify the provided detached signature over the provided message.
+    /// Returns `true` if the signature is valid.
+    fn verify(
+        &self,
+        agent_info: &AgentInfo,
+        message: &[u8],
+        signature: &[u8],
+    ) -> bool;
 }
 
-impl std::convert::TryFrom<AgentInfoStringTimestamps> for AgentInfo {
-    type Error = std::io::Error;
-    fn try_from(a: AgentInfoStringTimestamps) -> Result<Self, Self::Error> {
-        let created_at: i64 =
-            a.created_at.parse().map_err(std::io::Error::other)?;
-        let expires_at: i64 =
-            a.expires_at.parse().map_err(std::io::Error::other)?;
-        Ok(Self {
-            agent: a.agent,
-            space: a.space,
-            created_at: Timestamp::from_micros(created_at),
-            expires_at: Timestamp::from_micros(expires_at),
-            is_tombstone: a.is_tombstone,
-            metadata: a.metadata,
-        })
+mod serde_string_timestamp {
+    pub fn serialize<S>(
+        t: &crate::Timestamp,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&t.as_micros().to_string())
+    }
+
+    pub fn deserialize<'de, D>(
+        deserializer: D,
+    ) -> Result<crate::Timestamp, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s: &'de str = serde::Deserialize::deserialize(deserializer)?;
+        let i: i64 = s.parse().map_err(serde::de::Error::custom)?;
+        Ok(crate::Timestamp::from_micros(i))
     }
 }
 
-impl From<AgentInfo> for AgentInfoStringTimestamps {
-    fn from(a: AgentInfo) -> Self {
-        Self {
-            agent: a.agent,
-            space: a.space,
-            created_at: a.created_at.as_micros().to_string(),
-            expires_at: a.expires_at.as_micros().to_string(),
-            is_tombstone: a.is_tombstone,
-            metadata: a.metadata,
-        }
-    }
-}
-
-/// Agent information.
+/// AgentInfo stores metadata related to agents.
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
-#[serde(
-    rename_all = "camelCase",
-    try_from = "AgentInfoStringTimestamps",
-    into = "AgentInfoStringTimestamps"
-)]
+#[serde(rename_all = "camelCase")]
 pub struct AgentInfo {
     /// The agent id.
     pub agent: AgentId,
@@ -78,28 +58,28 @@ pub struct AgentInfo {
     pub space: SpaceId,
 
     /// When this metadata was created.
+    #[serde(with = "serde_string_timestamp")]
     pub created_at: Timestamp,
 
     /// When this metadata will expire.
+    #[serde(with = "serde_string_timestamp")]
     pub expires_at: Timestamp,
 
     /// If `true`, this metadata is a tombstone, indicating
     /// the agent has gone offline, and is no longer reachable.
     pub is_tombstone: bool,
 
-    /// Additional metadata associated with this agent.
-    pub metadata: AgentInfoMetadata,
-}
+    /// If set, this indicates the primary url at which this agent may
+    /// be reached. This should largely only be UNSET if this is a tombstone.
+    pub url: Option<String>,
 
-/// Defines a type capable of cryptographic signatures.
-pub trait Signer {
-    /// Sign the encoded data, returning the resulting detached signature,
-    /// or error.
-    fn sign(
-        &self,
-        agent_info: &AgentInfo,
-        encoded: &str,
-    ) -> BoxFut<'_, std::io::Result<bytes::Bytes>>;
+    /// If unset, this agent is claiming a zero storage arc,
+    /// that is, they are saying they store nothing.
+    /// If set, this indicates the inclusive bounds which this agent
+    /// claims they are an authority for storage. Note, if the first
+    /// bound is larger than the second bound, that means the claim wraps
+    /// around the end of u32::MAX to the other side.
+    pub storage_arc: Option<(u32, u32)>,
 }
 
 /// Signed agent information.
@@ -108,7 +88,7 @@ pub trait Signer {
 #[allow(clippy::manual_non_exhaustive)]
 pub struct AgentInfoSigned {
     /// The decoded information associated with this agent.
-    pub info: AgentInfo,
+    pub agent_info: AgentInfo,
 
     /// The encoded information that was signed.
     pub encoded: String,
@@ -125,16 +105,42 @@ impl AgentInfoSigned {
     /// Generate a signed agent info by signing an agent info.
     pub async fn sign<S: Signer>(
         signer: &S,
-        info: AgentInfo,
+        agent_info: AgentInfo,
     ) -> std::io::Result<std::sync::Arc<Self>> {
-        let encoded = serde_json::to_string(&info)?;
-        let signature = signer.sign(&info, &encoded).await?;
+        let encoded = serde_json::to_string(&agent_info)?;
+        let signature = signer.sign(&agent_info, encoded.as_bytes()).await?;
         Ok(std::sync::Arc::new(Self {
-            info,
+            agent_info,
             encoded,
             signature,
             _private: (),
         }))
+    }
+
+    /// Decode a canonical json encoding of a signed agent info.
+    pub fn decode<V: Verifier>(
+        verifier: &V,
+        encoded: &[u8],
+    ) -> std::io::Result<Self> {
+        #[derive(serde::Deserialize)]
+        #[serde(rename_all = "camelCase")]
+        struct Ref {
+            agent_info: String,
+            #[serde(with = "crate::serde_bytes_base64")]
+            signature: bytes::Bytes,
+        }
+        let v: Ref = serde_json::from_slice(encoded)?;
+        let agent_info: AgentInfo = serde_json::from_str(&v.agent_info)?;
+        if !verifier.verify(&agent_info, v.agent_info.as_bytes(), &v.signature)
+        {
+            return Err(std::io::Error::other("InvalidSignature"));
+        }
+        Ok(AgentInfoSigned {
+            agent_info,
+            encoded: v.agent_info,
+            signature: v.signature,
+            _private: (),
+        })
     }
 
     /// Get the canonical json encoding of this signed agent info.
@@ -158,7 +164,7 @@ impl std::ops::Deref for AgentInfoSigned {
     type Target = AgentInfo;
 
     fn deref(&self) -> &Self::Target {
-        &self.info
+        &self.agent_info
     }
 }
 
@@ -166,45 +172,69 @@ impl std::ops::Deref for AgentInfoSigned {
 mod test {
     use super::*;
 
-    pub struct TestSigner;
+    const SIG: &[u8] = b"fake-signature";
 
-    impl Signer for TestSigner {
+    struct TestCrypto;
+
+    impl Signer for TestCrypto {
         fn sign(
             &self,
             _agent_info: &AgentInfo,
-            _encoded: &str,
+            _encoded: &[u8],
         ) -> BoxFut<'_, std::io::Result<bytes::Bytes>> {
-            Box::pin(
-                async move { Ok(bytes::Bytes::from_static(b"fake-signature")) },
-            )
+            Box::pin(async move { Ok(bytes::Bytes::from_static(SIG)) })
+        }
+    }
+
+    impl Verifier for TestCrypto {
+        fn verify(
+            &self,
+            _agent_info: &AgentInfo,
+            _message: &[u8],
+            signature: &[u8],
+        ) -> bool {
+            signature == SIG
         }
     }
 
     #[tokio::test(flavor = "multi_thread")]
     async fn happy_encode_decode() {
+        let agent: AgentId = bytes::Bytes::from_static(b"test-agent").into();
+        let space: SpaceId = bytes::Bytes::from_static(b"test-space").into();
+        let now = Timestamp::from_micros(1731690797907204);
+        let later = Timestamp::from_micros(1731762797907204);
+        let url = Some("test-url".into());
+        let storage_arc = Some((42, u32::MAX / 13));
+
         let enc = AgentInfoSigned::sign(
-            &TestSigner,
+            &TestCrypto,
             AgentInfo {
-                agent: AgentId(crate::id::Id(bytes::Bytes::from_static(
-                    b"test-agent",
-                ))),
-                space: SpaceId(crate::id::Id(bytes::Bytes::from_static(
-                    b"test-space",
-                ))),
-                created_at: Timestamp::now(),
-                expires_at: Timestamp::now()
-                    + std::time::Duration::from_secs(60 * 60 * 20),
+                agent: agent.clone(),
+                space: space.clone(),
+                created_at: now,
+                expires_at: later,
                 is_tombstone: false,
-                metadata: AgentInfoMetadata {
-                    url1: Some("test-url".into()),
-                    storage_arc1: Some((42, u32::MAX / 13)),
-                },
+                url: url.clone(),
+                storage_arc,
             },
         )
         .await
         .unwrap()
         .encode()
         .unwrap();
-        println!("{enc}");
+
+        assert_eq!(
+            r#"{"agentInfo":"{\"agent\":\"dGVzdC1hZ2VudA\",\"space\":\"dGVzdC1zcGFjZQ\",\"createdAt\":\"1731690797907204\",\"expiresAt\":\"1731762797907204\",\"isTombstone\":false,\"url\":\"test-url\",\"storageArc\":[42,330382099]}","signature":"ZmFrZS1zaWduYXR1cmU"}"#,
+            enc
+        );
+
+        let dec = AgentInfoSigned::decode(&TestCrypto, enc.as_bytes()).unwrap();
+        assert_eq!(agent, dec.agent);
+        assert_eq!(space, dec.space);
+        assert_eq!(now, dec.created_at);
+        assert_eq!(later, dec.expires_at);
+        assert!(!dec.is_tombstone);
+        assert_eq!(url, dec.url);
+        assert_eq!(storage_arc, dec.storage_arc);
     }
 }
