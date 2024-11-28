169fefb46e35ca24b6f21e01ebfde1eae373003c
Jost Schulte <jost-s@users.noreply.github.com>
2024-11-28 13:26:27 -0600

    docs: udpate fetch queue description

--- crates/fetch_queue/src/lib.rs
+++ crates/fetch_queue/src/lib.rs
@@ -5,8 +5,7 @@
 //! fetched using the fetch queue.
 //!
 //! Gossip sessions are performed one after the other. Every session can have a set
-//! of missing ops as an outcome. Ops can be held by multiple peers in the network,
-//! so the fetch queue must keep track of which ops can be fetched from which peers.
+//! of missing ops as an outcome.
 //!
 //! The fetch queue holds sets of ops and sources where to fetch ops from. The actual
 //! fetching is executed by the fetch task.
