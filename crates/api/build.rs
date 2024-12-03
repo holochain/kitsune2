bbeca4e9bbe256744a04379cec3fce6c2f1074e7
neonphog <neonphog@gmail.com>
2024-12-03 09:23:11 -0700

    protoc-prebuilt

--- crates/api/build.rs
+++ crates/api/build.rs
@@ -1,7 +1,9 @@
-fn main() -> std::io::Result<()> {
-    std::env::set_var("PROTOC", protobuf_src::protoc());
+fn main() {
+    let (protoc_bin, _) = protoc_prebuilt::init("27.1")
+        .expect("Failed to initialize protoc_prebuilt binary");
+    std::env::set_var("PROTOC", protoc_bin);
     prost_build::Config::new()
         .bytes(["."])
-        .compile_protos(&["proto/wire.proto"], &["proto/"])?;
-    Ok(())
+        .compile_protos(&["proto/wire.proto"], &["proto/"])
+        .expect("Failed to compile protobuf protocol files");
 }
