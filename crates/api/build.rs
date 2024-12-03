c7c3632db3f838143cf3b0a1ea4583f0ffb04bb2
neonphog <neonphog@gmail.com>
2024-12-03 09:16:48 -0700

    review comment

--- crates/api/build.rs
+++ crates/api/build.rs
@@ -2,6 +2,6 @@ fn main() -> std::io::Result<()> {
     std::env::set_var("PROTOC", protobuf_src::protoc());
     prost_build::Config::new()
         .bytes(["."])
-        .compile_protos(&["src/wire.proto"], &["src/"])?;
+        .compile_protos(&["proto/wire.proto"], &["proto/"])?;
     Ok(())
 }
