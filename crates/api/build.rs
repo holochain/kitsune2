208fec56f52462139df53d97fe1f5dd53c231193
neonphog <neonphog@gmail.com>
2024-12-03 10:58:47 -0700

    protoc_prebuilt optional

--- crates/api/build.rs
+++ crates/api/build.rs
@@ -1,7 +1,11 @@
 fn main() {
-    let (protoc_bin, _) = protoc_prebuilt::init("27.1")
-        .expect("Failed to initialize protoc_prebuilt binary");
-    std::env::set_var("PROTOC", protoc_bin);
+    #[cfg(feature = "use_protoc_prebuilt")]
+    if std::env::var_os("PROTOC").is_none() {
+        let (protoc_bin, _) = protoc_prebuilt::init("27.1")
+            .expect("Failed to initialize protoc_prebuilt binary");
+        std::env::set_var("PROTOC", protoc_bin);
+    }
+
     prost_build::Config::new()
         .bytes(["."])
         .compile_protos(&["proto/wire.proto"], &["proto/"])
