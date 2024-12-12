2a66cb6866bf82f80e0f06fc05d407c050b2606b
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-12-12 11:14:12 +0000

    Update crates/api/src/doc/glossary.rs
    Co-authored-by: Jost Schulte <28270981+jost-s@users.noreply.github.com>

--- crates/api/src/doc/glossary.rs
+++ crates/api/src/doc/glossary.rs
@@ -1,7 +1,7 @@
 //! A glossary of terms used in the Kitsune2 project.
 //!
 //! ## Time slice
-//! A time slice is a period of time. It is defined to be a an interval from a start time,
+//! A time slice is a period of time. It is defined to be an interval from a start time,
 //! inclusive, up to some end time, exclusive.
 //!
 //! Time slices are used to group data by time, so that a single hash describing that data can be
