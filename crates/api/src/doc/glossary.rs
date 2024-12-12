46e15d10b765d26a7dac106c9cc5b971a7713044
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-12-12 11:18:29 +0000

    Tweak wording

--- crates/api/src/doc/glossary.rs
+++ crates/api/src/doc/glossary.rs
@@ -4,6 +4,6 @@
 //! A time slice is a period of time. It is defined to be an interval from a start time,
 //! inclusive, up to some end time, exclusive.
 //!
-//! Time slices are used to group data by time, so that a single hash describing that data can be
-//! used to check for consistency of that data with other peers.
+//! Time slices are used to group data by time, hashes of the data are combined so that a single
+//! hash describing that data can be used to check for consistency of that data with other peers.
 //!
