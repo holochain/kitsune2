ea81fdf8089a4febe4a8eda7ed48e75e213ad245
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-01-15 18:16:29 +0000

    Return op ids after processing

--- crates/api/src/op_store.rs
+++ crates/api/src/op_store.rs
@@ -76,7 +76,7 @@ pub trait OpStore: 'static + Send + Sync + std::fmt::Debug {
     fn process_incoming_ops(
         &self,
         op_list: Vec<Bytes>,
-    ) -> BoxFuture<'_, K2Result<()>>;
+    ) -> BoxFuture<'_, K2Result<Vec<OpId>>>;
 
     /// Retrieve a batch of ops from the host by time range.
     ///
