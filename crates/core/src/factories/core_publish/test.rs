76a7b967d264e4d479bddb98f8955f6e5e88cd75
Matthias <tuttiacc@hotmail.com>
2025-02-12 22:53:25 +0100

    change test that checks that publishing to non-existing url does not break publishing flow

--- crates/core/src/factories/core_publish/test.rs
+++ crates/core/src/factories/core_publish/test.rs
@@ -4,9 +4,7 @@ use kitsune2_api::{
     DynOpStore, Publish, SpaceHandler, Timestamp, TxBaseHandler, TxHandler,
     TxSpaceHandler, Url,
 };
-use kitsune2_test_utils::{
-    enable_tracing, id::create_op_id_list, iter_check, space::TEST_SPACE_ID,
-};
+use kitsune2_test_utils::{enable_tracing, iter_check, space::TEST_SPACE_ID};
 
 use crate::{
     default_test_builder,
@@ -106,18 +104,53 @@ async fn published_ops_can_be_retrieved() {
 }
 
 #[tokio::test(flavor = "multi_thread")]
-async fn publish_to_invalid_url_does_not_error() {
+async fn publish_to_invalid_url_does_not_impede_subsequent_publishes() {
     enable_tracing();
 
-    let (core_publish_1, _, _) =
+    let (core_publish_1, op_store_1, _) =
+        setup_test(&CorePublishConfig::default()).await;
+    let (_core_publish2, op_store_2, url_2) =
         setup_test(&CorePublishConfig::default()).await;
 
-    let op_ids = create_op_id_list(2);
+    let incoming_op_1 = MemoryOp::new(Timestamp::now(), vec![1]);
+    let incoming_op_id_1 = incoming_op_1.compute_op_id();
+    let incoming_op_2 = MemoryOp::new(Timestamp::now(), vec![2]);
+    let incoming_op_id_2 = incoming_op_2.compute_op_id();
 
-    core_publish_1
-        .publish_ops(op_ids, Url::from_str("ws://notanexistingurl:80").unwrap())
+    op_store_1
+        .process_incoming_ops(vec![incoming_op_1.into(), incoming_op_2.into()])
         .await
         .unwrap();
 
-    tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+    let op_ids = vec![incoming_op_id_1.clone(), incoming_op_id_2.clone()];
+
+    // Publish to a non-existing Url
+    core_publish_1
+        .publish_ops(
+            op_ids.clone(),
+            Url::from_str("ws://notanexistingurl:80").unwrap(),
+        )
+        .await
+        .unwrap();
+
+    // Publish to an existing Url to verify that the prior publishing
+    // to a non-existing Url did not cause an error that would
+    // break the publishing flow
+    core_publish_1.publish_ops(op_ids, url_2).await.unwrap();
+
+    let ops = iter_check!(1000, {
+        let ops = op_store_2
+            .retrieve_ops(vec![
+                incoming_op_id_1.clone(),
+                incoming_op_id_2.clone(),
+            ])
+            .await
+            .unwrap();
+
+        if ops.len() == 2 {
+            return ops;
+        }
+    });
+
+    assert!(ops.len() == 2);
 }
