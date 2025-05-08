672c7f94f5d77427bb588cc0d981a522026ec8da
neonphog <neonphog@gmail.com>
2025-05-08 16:42:23 -0600

    forgot the hook server config

--- docker/kitsune2_bootstrap_srv_auth_test/start-cmd.sh
+++ docker/kitsune2_bootstrap_srv_auth_test/start-cmd.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 test-auth-hook-server 8080 &
-kitsune2-bootstrap-srv --production --tls-cert /etc/bootstrap_srv/cert.pem --tls-key /etc/bootstrap_srv/key.pem
+kitsune2-bootstrap-srv --production --tls-cert /etc/bootstrap_srv/cert.pem --tls-key /etc/bootstrap_srv/key.pem --authentication-hook-server http://127.0.0.1:8080/authenticate
