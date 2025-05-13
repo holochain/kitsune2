c03ef0b691f0a59c45d9894d3cef6fa372c4c4fd
Callum Dunster <cdunster@users.noreply.github.com>
2025-05-13 11:46:51 +0200

    feat(showcase): print logs to file with PID in name

--- crates/kitsune2_showcase/src/main.rs
+++ crates/kitsune2_showcase/src/main.rs
@@ -30,6 +30,14 @@ const COMMAND_LIST: &[(&str, &str)] = &[
 ];
 
 fn main() {
+    let pid = std::process::id();
+    let file_appender = tracing_appender::rolling::never(
+        "./logs",
+        format!("{}-{}.log", env!("CARGO_PKG_NAME"), pid),
+    );
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    tracing_subscriber::fmt().with_writer(non_blocking).init();
+
     let _ = rustls::crypto::aws_lc_rs::default_provider().install_default();
 
     let args = <Args as clap::Parser>::parse();
