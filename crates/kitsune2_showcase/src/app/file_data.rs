7bc54e815114ccdad20bd7a2a5fbbe5cb6e9e643
Callum Dunster <cdunster@users.noreply.github.com>
2025-05-15 18:46:12 +0200

    feat(showcase): store file data instead of strings

---
+++ crates/kitsune2_showcase/src/app/file_data.rs
@@ -0,0 +1,7 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct FileData {
+    /// The name of the file to be stored
+    pub name: String,
+}
