1368aa156235ce570f97fd7cd1cdcca8aa3c2e35
neonphog <neonphog@gmail.com>
2025-09-03 15:00:14 -0600

    initial fetch reporting api trait

---
+++ crates/api/src/report.rs
@@ -0,0 +1,41 @@
+//! Kitsune2 report types.
+
+use crate::{builder, config, BoxFut, K2Result, OpId, SpaceId, Url};
+use std::sync::Arc;
+
+/// Trait for implementing a report module in kitsune2.
+pub trait Report: 'static + Send + Sync + std::fmt::Debug {
+    /// Notify that we have fetched op data from a remote peer.
+    fn fetched_op(
+        &self,
+        _space_id: SpaceId,
+        _source: Url,
+        _op_id: OpId,
+        _size_bytes: u32,
+    ) -> BoxFut<'_, K2Result<()>> {
+        // provided impl is a no-op
+        Box::pin(async move { Ok(()) })
+    }
+}
+
+/// Trait object [Report].
+pub type DynReport = Arc<dyn Report>;
+
+/// A factory for creating report instances.
+pub trait ReportFactory: 'static + Send + Sync + std::fmt::Debug {
+    /// Help the builder construct a default config from the chosen
+    /// module factories.
+    fn default_config(&self, config: &mut config::Config) -> K2Result<()>;
+
+    /// Validate configuration.
+    fn validate_config(&self, config: &config::Config) -> K2Result<()>;
+
+    /// Construct a Report instance.
+    fn create(
+        &self,
+        builder: Arc<builder::Builder>,
+    ) -> BoxFut<'static, K2Result<DynReport>>;
+}
+
+/// Trait object [ReportFactory].
+pub type DynReportFactory = Arc<dyn ReportFactory>;
