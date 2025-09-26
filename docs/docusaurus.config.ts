de62d8e093740dd11b94b5d028e5745938dd0eb3
Paul d'Aoust <paul.daoust@holo.host>
2025-09-26 12:45:45 -0700

    fix: change to kitsune2.holochain.org

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://kitsune2.pages.dev',
+  url: 'https://kitsune2.holochain.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
