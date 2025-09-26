9f5769c6623db3b3408ae66b2ac48d9f96db020b
Paul d'Aoust <paul.daoust@holo.host>
2025-09-26 12:29:55 -0700

    fix: temporarily set root URL to kitsune2.pages.dev

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://kitsune2.github.io',
+  url: 'https://kitsune2.pages.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
