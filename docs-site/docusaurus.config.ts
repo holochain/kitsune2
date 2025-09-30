f4c46106acbe76b4728560c1f964c3cd66be7749
Paul d'Aoust <paul.daoust@holo.host>
2025-09-30 12:10:41 -0700

    fix: remove unused GitHub Pages config

--- docs-site/docusaurus.config.ts
+++ docs-site/docusaurus.config.ts
@@ -20,11 +20,6 @@ const config: Config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'holochain', // Usually your GitHub org/user name.
-  projectName: 'kitsune2', // Usually your repo name.
-
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'throw',
 
