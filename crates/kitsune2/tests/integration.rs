0d8c6f71af345ff62856310ebfde5d729f9cf0e6
Nicolas Luck <nicolas@lucksus.eu>
2026-02-23 19:51:35 +0100

    fix: unflake testst. bootstrap default min backoff is 5s, override with 1s.

--- crates/kitsune2/tests/integration.rs
+++ crates/kitsune2/tests/integration.rs
@@ -99,6 +99,7 @@ async fn make_kitsune_node(
         .set_module_config(&CoreBootstrapModConfig {
             core_bootstrap: CoreBootstrapConfig {
                 server_url: Some(bootstrap_server_url.to_owned()),
+                backoff_min_ms: 1000,
                 backoff_max_ms: 1000,
                 ..Default::default()
             },
